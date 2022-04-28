8ddca59464481c96b5d72ae1d08c96086bc631da
Muhil Sathish kumar <muhil.sathish_kofee@yahoo.com>
2022-04-28 16:01:01 +0530

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Dina:pixelsize=9:antialias=true:autohint=true";
-static char *font2[] = { "Dina :pixelsize=20:antialias=true:autohint=true" };
+static char *font = "Dina:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = { "Dina :pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 24;
 
 /*
